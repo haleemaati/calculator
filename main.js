9673e93643361bbf755f1bffdc852fc8b1acc2de
haleemati <haleemajusi2@gmail.com>
2023-09-05 12:00:51 -1200

    calculator

---
+++ main.js
@@ -0,0 +1,17 @@
+// display btn content in calculator
+function display(content){
+    result.value +=content
+}
+function allclear(){
+    result.value=""
+}
+function exprEval(){
+    try{
+        result.value = eval(result.value)
+    }catch{
+        result.value = "invalid expression!!!"
+    }
+}
+function removeLast(){
+    result.value = result.value.slice(0,-1)
+}
